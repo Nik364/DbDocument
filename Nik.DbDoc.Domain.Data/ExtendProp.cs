18c8f437d63046306fdf78a8fe252d7a6865124a
zhubaokeji <zhubaokeji@DESKTOP-77PIG03>
2017-05-16 10:26:37 +0800

    【ADD】添加ExtendProp

---
+++ Nik.DbDoc.Domain.Data/ExtendProp.cs
@@ -0,0 +1,39 @@
+﻿namespace Nik.DbDoc.Domain.Data
+{
+    /// <summary>
+    /// 数据库扩展属性访问类
+    /// </summary>
+    public class ExtendProp : Base
+    {
+        /// <summary>
+        /// 修改扩展属性
+        /// </summary>
+        /// <param name="dbname">数据库名称</param>
+        /// <param name="prop">数据库属性</param>
+        public void Update(string dbname, ExtendProp prop)
+        {
+
+        }
+
+
+        /// <summary>
+        /// 添加扩展属性
+        /// </summary>
+        /// <param name="dbname"></param>
+        /// <param name="prop"></param>
+        public void Add(string dbname, ExtendProp prop)
+        {
+        }
+
+        /// <summary>
+        /// 扩展属性是否存在
+        /// </summary>
+        /// <param name="dbname"></param>
+        /// <param name="prop"></param>
+        /// <returns></returns>
+        public bool Exist(string dbname, ExtendProp prop)
+        {
+            return true;
+        }
+    }
+}
---
+++ Nik.DbDoc.Domain.Model/ExtendProp.cs
@@ -0,0 +1,48 @@
+﻿namespace Nik.DbDoc.Domain.Model
+{
+    /// <summary>
+    /// 数据库扩展属性实体
+    /// </summary>
+    public class ExtendProp
+    {
+        /// <summary>
+        /// 扩展属性名
+        /// </summary>
+        internal string Name { get; set; } = "MS_Description";
+        
+        /// <summary>
+        /// 架构类型
+        /// </summary>
+        internal string SchemaType { get; set; } = "SCHEMA";
+
+        /// <summary>
+        /// 表类型
+        /// </summary>
+        internal string TableType { get; set; } = "TABLE";
+
+        /// <summary>
+        /// 列类型
+        /// </summary>
+        internal string ColumnType { get; set; } = "COLUMN";
+
+        /// <summary>
+        /// 扩展属性值
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// 架构名
+        /// </summary>
+        public string SchemaName { get; set; } = "dbo";
+
+        /// <summary>
+        /// 表名
+        /// </summary>
+        public string TableName { get; set; }
+
+        /// <summary>
+        /// 列名
+        /// </summary>
+        public string ColumnName { get; set; }
+    }
+}
